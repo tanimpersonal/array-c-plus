df3f5de1e1b648554bf9ab3832ae400fa929f2c1
tanimpersonal <tashintanim2019@gmail.com>
2022-08-23 04:38:52 +0600

    'swap'

---
+++ swap.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int a[]= {2,4,1,5,6}, n=5;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[i]>a[j]){
+                //manual way
+                /*int temp= a[i];
+                a[i]= a[j];
+                a[j]=temp;*/
+                //library functions way
+                swap(a[i],a[j]);
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout << a[i] << endl;
+    }
+    return 0;
+}
